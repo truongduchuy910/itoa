f78fdd10830ee83f4d147286186aee54daa781dd
unknown <truongduchuy910@gmail.com>
2022-08-09 16:34:09 +0700

    add public script

---
+++ public.sh
@@ -0,0 +1,42 @@
+cd ../access-control/
+npm publish --access public
+cd ../adapter-mongoose/
+npm publish --access public
+cd ../app-admin-ui/
+npm publish --access public
+cd ../app-graphql/
+npm publish --access public
+cd ../app-graphql-playground/
+npm publish --access public
+cd ../app-schema-router/
+npm publish --access public
+cd ../app-static/
+npm publish --access public
+cd ../app-version/
+npm publish --access public
+cd ../auth-password/
+npm publish --access public
+cd ../field-views-loader/
+npm publish --access public
+cd ../fields/
+npm publish --access public
+cd ../fields-authed-relationship/
+npm publish --access public
+cd ../fields-color/
+npm publish --access public
+cd ../fields-mongoid/
+npm publish --access public
+cd ../file-adapters/
+npm publish --access public
+cd ../itoa/
+npm publish --access public
+cd ../list-plugins/
+npm publish --access public
+cd ../server-side-graphql-client/
+npm publish --access public
+cd ../session/
+npm publish --access public
+cd ../test-utils/
+npm publish --access public
+cd ../utils/
+npm publish --access public
