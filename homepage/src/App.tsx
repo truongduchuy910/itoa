7135b8467b51cb0f7f55b437b477e6de3bcdb592
unknown <truongduchuy910@gmail.com>
2022-08-10 21:07:48 +0700

    theme

--- homepage/src/App.tsx
+++ homepage/src/App.tsx
@@ -7,7 +7,19 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Itoa.js</h1>
+      <h1 style={{ textTransform: 'uppercase' }}>
+        I<span style={{ color: 'var(--color-delta)' }}>to</span>a
+      </h1>
+      <h2>Intelligent to archivement</h2>
+      <ul>
+        <li>Trần Ngọc Huy - backend</li>
+        <li>Nguyễn Tấn Hiệp - fontend</li>
+        <li>Nguyễn Huỳnh Quang - tester</li>
+      </ul>
+      <hr />
+      <footer>
+        <code>copyright of itoa coporation 2022</code>
+      </footer>
     </div>
   );
 }
